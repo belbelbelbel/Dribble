1ded3c1c48ed1d0e056723b903ffe42cd289d293
belbelbelbel <gronaldchia@gmail.com>
2024-06-19 00:33:34 +0100

    Coupons begins

--- src/NvaBar.tsx
+++ src/NvaBar.tsx
@@ -1,47 +1,51 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
 import { FaCartArrowDown } from "react-icons/fa";
+import { MdKeyboardArrowDown } from "react-icons/md";
 
 
 export const NvaBar = () => {
     return (
-        <div className='w-screen bg-white h-[6rem] overflow-hidden  text-black'>
-            <div className='w-[80%] py-[1.8vw] justify-between flex mx-auto items-center'>
-                <div className='flex items-center gap-[6rem]'>
-                    <div className='tracking-[2px] text-[1.5vw]' style={{ fontFamily: "Open Sans" }}>
-                        C<span className='text-red-400'>o</span>U<span className='text-red-400'>p</span>O<span className='text-red-400'>n</span>S
+        <div className='w-screen bg-white h-[6rem] overflow-hidden  text-black navbar-cont z-40'>
+            <div className='w-[80%] py-[1.3vw] justify-between flex mx-auto items-center'>
+                <div className='flex items-center gap-[4vw]'>
+                    <div className='tracking-[2px] text-[#383838] text-[1.3vw]' style={{ fontFamily: "Open Sans" }}>
+                        C<span className='text-red-400 text-[1.8vw]  mx-0'>O</span>UPONS
                     </div>
                     
 
-                    <div className='flex gap-[3rem]'>
-                        <div className='text-red-400 text-center'>
+                    <div className='flex gap-[2rem] text-[0.9vw] font-normal text-[#383838]'>
+                        <div className='text-red-600  text-center'>
                             <Link to="/">Home</Link>
                         </div>
                         <div>
-                            <Link to="/">Home</Link>
+                            <div className='flex  items-center'>
+                                <div><Link to="/">Categories</Link></div>
+                                <div className='relative top-[1.3px] cursor-pointer'><MdKeyboardArrowDown className='text-[1.2vw]'/></div>
+                            </div>
                         </div>
                         <div>
-                            <Link to="/">Home</Link>
+                            <Link to="/">Packages</Link>
                         </div>
                         <div>
-                            <Link to="/">Home</Link>
+                            <Link to="/">Discounted Services</Link>
                         </div>
                         <div>
-                            <Link to="/">Home</Link>
+                            <Link to="/">Individual products</Link>
                         </div>
                         <div>
-                            <Link to="/">Home</Link>
+                            <Link to="/">Partner with us</Link>
                         </div>
                     </div>
                 </div>
                 <div className='flex items-center gap-[1vw]'>
                     <div className='flex items-center gap-[0.7vw]'>
-                        <div><FaCartArrowDown className='text-red-300' /></div>
-                        <div className='text-red-300'>Cart</div>
+                        <div><FaCartArrowDown className='text-red-500' /></div>
+                        <div className='text-red-400'>Cart</div>
                     </div>
                     <div className='h-6 w-0.5 bg-gray-500'></div>
                     <div className=''>
-                        <div className='w-[30px] h-[30px]  bg-black rounded-full'></div>
+                        <div className='w-[30px] h-[30px]  bg-[#383838] rounded-full'></div>
                     </div>
                 </div>
             </div>
--- src/Pages/Header.tsx
+++ src/Pages/Header.tsx
@@ -1,8 +1,8 @@
 import React from 'react'
-
+import "../Style/Styles.css"
 export const Header = () => {
   return (
-    <div className='w-screen h-[50vh] bg-red-100'>
+    <div className='w-screen h-[60vh] pink-bg z-10'>
 
     </div>
   )
