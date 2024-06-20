ea813dd9fe1a9114b0de8ddaea7e0d38f589f8ad
belbelbelbel <gronaldchia@gmail.com>
2024-06-21 00:24:06 +0100

    fixed merged images to be pushed

--- src/Pages/Context1.tsx
+++ src/Pages/Context1.tsx
@@ -2,15 +2,15 @@ import React from 'react'
 
 export const Content1 = () => {
   return (
-    <div className='relative w-screen h-[60vw]  mx-auto items-center justfiy-center flex '>
+    <div className='relative w-screen h-screen  mx-auto items-center justfiy-center flex '>
       <div className='w-[80%] mx-auto flex items-center gap-[4vw] flex-col'>
         <div className='text-center flex-col flex gap-[0.8vw]'>
           <h1 className='font-bold text-black text-[2vw]'>Categories</h1>
-          <div className='text-[0.7vw] text-gray-500'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quod porro dolores nihil minus aperiam, eum quam, earum expedita,<br /> aspernatur suscipit ex? Atque, nesciunt vel quibusdam reprehenderit animi pariatur temporibus debitis?</div>
+          <div className='text-[0.7vw] text-gray-500 w-[40%] mx-auto'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quod porro dolores nihil minus aperiam, eum quam, earum expedita, aspernatur suscipit ex? Atque, nesciunt vel quibusdam reprehenderit animi pariatur temporibus debitis?</div>
         </div>
         <div className='flex gap-[2vw] items-center '>
-          
-            <div className='h-[21vw] w-[25vw] pink-bg rounded-[1vw]'>
+
+          <div className='h-[21vw] w-[25vw] pink-bg rounded-[1vw]'>
             <div className=' flex flex-col gap-[0.5vw] w-[25vw] h-[13vw] items-center rounded-[2vw]'>
               <img src="/content2.jpg" alt="imagefor event" className='rounded-[1vw] h-[12vw] w-full object-cover' />
               <div className='flex items-start flex-col justify-center   gap-[0.1vw] mx-[1vw]'>
--- src/Pages/Header.tsx
+++ src/Pages/Header.tsx
@@ -1,31 +1,41 @@
 import React from 'react'
+import { CiSearch } from "react-icons/ci";
+import { IoLocationOutline } from "react-icons/io5";
 import "../Style/Styles.css"
 export const Header = () => {
     return (
-        <div className='w-screen h-scr text-black z-20'>
+        <div className='w-screen  text-black z-20'>
             <div className='w-screen h-[35vw] pink-bg flex flex-col justify-center items-center '>
-                <div className='relative z-50  flex flex-col font-bold w-[80%] leading-[5vw]  mx-auto'>
-                    <div className='w-[45%] body text-[4vw] tracking-[1.5px]' >
-                        Save upto <div className='flex gap-[0.4vw]' style={{ fontFamily: "Open sans" }}><div className='text-red-500'>80%</div> with</div> BendeeHaven
+                <div className='relative z-50  flex flex-col font-bold w-[80%] leading-[5vw]   mx-auto'>
+                    <div className='w-[45%] body text-[5vw] tracking-[2px]' >
+                        Save upto <div className='flex gap-[0.4vw]' style={{ fontFamily: "Open sans" }}><div className='text-red-400'>80%</div> with</div> B<span className=' text-red-400'>e</span>nd<span className=' text-red-400'>e</span>Hav<span className=' text-red-500'></span>n
                     </div>
                     <div className='text-[0.9vw] font-light text-gray-500 relative -top-[1vw]'>
                         Lorem ipsum dolor sit amet consectetur adipisicing elit. Blanditiis consectetur adipisicing
                     </div>
-                    <div className=' w-[50%] flex  h-full justify-between flex items-center gap-[3vw]'>
+                    <div className=' w-[50%] flex    flex items-center gap-[1vw]'>
 
-                        <div className='bg-white w-[90%] px-[1vw] h-[3vw] rounded-[2vw] gap-[1vw] flex items-center '>
-                            <div className= "text-[1vw]">
-                                <div>jbewlhdjqwq</div>
+                        <div className='bg-white w-[90%] px-[1vw] h-[3vw] rounded-[2vw] gap-[0vw] flex items-center '>
+                            <div className="text-[1vw]">
+                                <CiSearch className='text-[1.6vw] text-red-500' />
                                 <hr />
                             </div>
-                            <input type="text" className=' w-[70%] bg-transparent text-[1vw] outline-none  h-full py-[0.5vw] rounded-[2vw] px-[1vw]' />
+                            <input type="text" placeholder='Search' autoFocus className=' w-[70%] bg-transparent text-[1vw] outline-none  font-medium  h-full py-[0.5vw] rounded-[2vw] px-[1vw]' />
+                          <div className='flex items-center gap-[0.5vw]'>
+                            <div className='h-6 w-0.5 bg-gray-300'></div>
+                            <div><IoLocationOutline className='text-red-500 text-[1.2vw]'/></div>
+                          <div className='text-[1vw] font-normal text-red-500'>Location</div>
+                          </div>
                         </div>
-                        <button className= "text-[1vw] bg-red-500 px-[1vw] h-full  ">sadnckdjas,mnc</button>
+                        <div className='items-center justify-center relative flex'>
+                            <button className="text-[1.2vw] bg-red-400 h-full px-[3vw] font-normal text-white flex items-center rounded-[2vw] justify-center h-0 py-[1.4vw] search-bar" style={{fontFamily:"Open sans"}}>Explore</button>
+                        </div>
                     </div>
                     {/* <img src="/genelify-667417c93b261-signature-generator (2).png" alt="the opening" width="300px"/> */}
                 </div>
-
-                <img src="/wave (10).svg" alt="wavy flow" className='absolute -bottom-[7vw] -rotate-270  z-0 left-0  ' />
+                <div>
+                    <img src="/wave (10).svg" alt="wavy flow" className='absolute -bottom-[7vw] -rotate-270  z-0 left-0  ' />
+                </div>
             </div>
             <div className='relatiive'>l;wejmrst;lkgjwe;ms;ldt.fe</div>
         </div>
