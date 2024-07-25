e0af73e26436e46b30d254ae5c441ae6668b81be
belbelbelbel <gronaldchia@gmail.com>
2024-07-25 04:32:21 +0100

    subtle  desgin

--- src/Pages/Context1.tsx
+++ src/Pages/Context1.tsx
@@ -1,4 +1,4 @@
-import  { useState, useEffect } from 'react';
+import  { useState } from 'react';
 import { FaAngleRight, FaAngleLeft } from "react-icons/fa6";
 import 'react-multi-carousel/lib/styles.css';
 export const Content1 = () => {
